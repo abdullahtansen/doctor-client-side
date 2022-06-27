a946cdf25e9ed357f5f884621d152966ccad6987
abdullahtansen <abdullahaltansen899@gmail.com>
2022-06-27 15:18:26 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes,Route} from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/home" element={<Home/>} />
       <Route path="/appointment" element={<Appointment/>} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,64 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import footerbg from "../../../images/footer.png";
+
+const header = {
+  fontSize: "20px",
+  fontWeight: "700",
+  color: "green",
+  padding: "5px",
+};
+const footerBackground = {
+  background: `url(${footerbg})`,
+  backgroundColor: "rgba(25, 20, 0, 0.5)",
+  backgroundBlendMode: "darken,luminosity",
+};
+const service = {
+  color: "black",
+  fontWeight: "400",
+  fontSize: "16px",
+};
+
+const Footer = () => {
+  return (
+    <Container sx={{ mt: 2, pb: 2 }} style={footerBackground}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={4} md={4} sm={12}>
+            <Typography style={header} variant="h5">
+              SERVICES
+            </Typography>
+            <Typography style={service}>Emergency Checkup</Typography>
+            <Typography style={service}>Monthly Checkup</Typography>
+            <Typography style={service}>Weekly Checkup</Typography>
+            <Typography style={service}>Deep Checkup</Typography>
+          </Grid>
+          <Grid item xs={4} md={4} sm={12}>
+            <Typography style={header} variant="h5">
+              ORAL HEALTH
+            </Typography>
+            <Typography style={service}>Flouride Treatment</Typography>
+            <Typography style={service}>Cavity Filling</Typography>
+            <Typography style={service}>Teath whitening</Typography>
+          </Grid>
+          <Grid item xs={4} md={4} sm={12}>
+            <Typography style={header} variant="h5">
+              OUR ADDRESS
+            </Typography>
+            <Typography style={service}>Mirpur-13,Dhaka</Typography>
+          </Grid>
+        </Grid>
+        <Typography>
+          Copyright 2022 All Rights Reserved By{" "}
+          <span
+            style={{ color: "yellow", fontSize: "22px", fontWeight: "500" }}
+          >
+            Tansen
+          </span>
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Footer;
